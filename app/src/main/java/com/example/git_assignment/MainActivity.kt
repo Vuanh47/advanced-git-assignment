9eb845eb9647b18d59fe68b7362d9fcb29b0a9d8
vuanh47 <daovuanh2207@gmail.com>
2026-06-20 19:31:42 +0700

    Feature: Add hello button with toast message

--- app/src/main/java/com/example/git_assignment/MainActivity.kt
+++ app/src/main/java/com/example/git_assignment/MainActivity.kt
@@ -1,29 +1,39 @@
 package com.example.git_assignment
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.git_assignment.ui.theme.GitassignmentTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             GitassignmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
@@ -32,11 +42,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+            Toast.makeText(
+                context,
+                "Xin chào",
+                Toast.LENGTH_SHORT
+            ).show()
+        },
         modifier = modifier
-    )
+    ) {
+        Text(text = "Hello $name")
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
